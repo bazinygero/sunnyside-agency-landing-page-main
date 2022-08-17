4f7de4db4cb6c1dc0caf9608b6b513d2a3f93455
bazinygero <97780599+bazinygero@users.noreply.github.com>
2022-08-18 01:36:02 +0300

    Add mobile styling.

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,21 @@
-console.log('HELLO');
+const nav = document.querySelector('nav');
+const burger = document.querySelector('.burger-btn');
+const navlist = nav.querySelector('ul');
 
-const test = () => {
-	console.log('this is a test');
-};
+function mobileNav(a) {
+    nav.classList[a ? 'add' : 'remove']('mobile-menu');
+    if (nav.classList.contains('mobile-menu')) {
+        navlist.style.display = 'none';
+        burger.addEventListener('click', function(){
+
+            navlist.style.display = navlist.style.display === 'none' ? 'flex' : 'none';
+        });
+    } else {
+        navlist.style.display = 'flex';
+    }
+}
+      
+window.onresize = function() {
+    mobileNav(this.innerWidth < 697);  
+}
+window.onresize();
